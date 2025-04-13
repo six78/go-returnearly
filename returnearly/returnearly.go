6b98801197c5acb9e85e4aaa373a23810b8ede8c
Igor Sirotin <igor.sirotin.1012@gmail.com>
2025-04-13 13:25:45 +0100

    feat: logging

--- returnearly/returnearly.go
+++ returnearly/returnearly.go
@@ -4,77 +4,92 @@ import (
 	"flag"
 	"fmt"
 	"go/ast"
+	"io"
+	"log"
+	"os"
 	"reflect"
 
 	"golang.org/x/tools/go/analysis"
 )
 
-var Analyzer = &analysis.Analyzer{
-	Name: "returnearly",
-	Doc:  "suggests inverting conditions to follow the return early pattern",
-	Run:  run,
-}
+var (
+	Analyzer = &analysis.Analyzer{
+		Name: "returnearly",
+		Doc:  "suggests inverting conditions to follow the return early pattern",
+		Run:  run,
+		Flags: func() flag.FlagSet {
+			fs := flag.NewFlagSet("returnearly", flag.ExitOnError)
+			fs.BoolVar(&verboseOutput, "verbose", false, "enable verbose output")
+			return *fs
+		}(),
+	}
+
+	verboseOutput = false
+	logger        = log.New(io.Discard, "", 0)
+)
 
 func position(pass *analysis.Pass, node ast.Node) string {
 	if node == nil || reflect.ValueOf(node).IsNil() {
 		return "nil"
 	}
-	//if pass == nil {
-	//	return ""
-	//}
-	//if pass.Fset == nil {
-	//	return ""
-	//}
-	p := node.Pos()
-	pos := pass.Fset.Position(p)
+	pos := pass.Fset.Position(node.Pos())
 	return fmt.Sprintf("%s:%d", pos.Filename, pos.Line)
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	if verboseOutput {
+		logger.SetFlags(log.LstdFlags | log.Lshortfile)
+		logger.SetOutput(os.Stdout)
+	}
+
 	for _, file := range pass.Files {
+		if ast.IsGenerated(file) {
+			continue
+		}
+
 		// Create a stack to track parent nodes
 		parentStack := &ParentStack{}
 
+		// Traverse the node
 		ast.Inspect(file, func(n ast.Node) bool {
 			// Pop the stack when exiting a node
 			if n == nil {
-				fmt.Printf("- pop parent\n")
 				parentStack.Pop()
 				return true
 			}
 
+			// Analyze the node
+			result := analyzeNode(pass, file, n, parentStack)
+
 			// Push the current node to the stack
-
-			result := inspectNode(pass, file, n, parentStack)
-
-			fmt.Printf("- push parent: type = %s, value = %v\n", reflect.TypeOf(n), n)
 			parentStack.Push(n)
 
 			return result
 		})
 	}
+
 	return nil, nil
 }
 
-func inspectNode(pass *analysis.Pass, file *ast.File, n ast.Node, stack *ParentStack) bool {
+func analyzeNode(pass *analysis.Pass, file *ast.File, n ast.Node, stack *ParentStack) bool {
 	ifStmt, report := n.(*ast.IfStmt)
 	if !report || ifStmt.Body == nil || ifStmt.Else != nil {
 		return true // we only care about if-without-else
 	}
 
-	fmt.Println("------------")
-	fmt.Printf("found condition: %s\n", position(pass, ifStmt))
+	logger.Println("------------")
+	logger.Printf("found condition: %s\n", position(pass, ifStmt))
 
 	// Get the direct parent of the `ifStmt` from the stack
 	parentBlock, parentFunc, isFuncBody := stack.ParentInfo()
 	if parentBlock == nil {
-		fmt.Println("no enclosing block found")
+		logger.Println("no enclosing block found")
 		return true
 	}
 
 	//isFuncBody := stack.IsFuncBody()
 	//if !isFuncBody {
-	//	fmt.Println("Block is not part of a function body")
+	//	logger.Println("Block is not part of a function body")
 	//	return true
 	//}
 
@@ -82,13 +97,12 @@ func inspectNode(pass *analysis.Pass, file *ast.File, n ast.Node, stack *ParentS
 
 	//// If no parentBlock block is found, skip the node
 	//if parentBlock == nil {
-	//	fmt.Printf("no parent block found\n")
+	//	logger.Printf("no parent block found\n")
 	//	return true
 	//}
 
 	// Find statements that follow the if block in the parent block
 	//parentBlock, parentFunc := findEnclosingBlock(pass, file, ifStmt)
-	fmt.Printf("- parentBlock: %s\n", position(pass, parentBlock))
 	//if parentBlock == nil {
 	//	return true
 	//}
@@ -134,7 +148,7 @@ func inspectNode(pass *analysis.Pass, file *ast.File, n ast.Node, stack *ParentS
 	//	//afterLines := stmtEnd.Line - stmtStart.Line
 	//	//afterLen += afterLines
 	//
-	//	//fmt.Printf("- parent list iteration %d: %s, added = %d, total = %d\n", i, position(pass, stmt), afterLines, afterLen)
+	//	//logger.Printf("- parent list iteration %d: %s, added = %d, total = %d\n", i, position(pass, stmt), afterLines, afterLen)
 	//
 	//	if isTerminalStmt(stmt) {
 	//		afterTerminates = true
@@ -151,12 +165,10 @@ func inspectNode(pass *analysis.Pass, file *ast.File, n ast.Node, stack *ParentS
 	bodyTerminates := blockEndsWithTerminal(ifStmt.Body.List)
 	report = thenLen > afterLen && (bodyTerminates || emptyAfter) && afterTerminates
 
-	fmt.Printf("- THEN block: start = %d, end = %d, len = %d\n", thenStart, thenEnd, thenLines)
-	fmt.Printf("- AFTER block: start = %d, end = %d, len = %d\n", afterStart, afterEnd, afterLen)
-	fmt.Printf("- TERMINATES: body = %v, after = %v\n", bodyTerminates, afterTerminates)
-	fmt.Printf("- parentFunc: %v\n", parentFunc)
-	fmt.Printf("- isFuncBody: %v\n", isFuncBody)
-	fmt.Printf("- report: %v\n", report)
+	logger.Printf("- THEN block: start = %d, end = %d, len = %d\n", thenStart, thenEnd, thenLines)
+	logger.Printf("- AFTER block: start = %d, end = %d, len = %d\n", afterStart, afterEnd, afterLen)
+	logger.Printf("- TERMINATES: body = %v, after = %v\n", bodyTerminates, afterTerminates)
+	logger.Printf("- parentFunc = %v, isFuncBody = %v, report = %v\n", parentFunc, isFuncBody, report)
 
 	if report {
 		pass.Reportf(ifStmt.Pos(), "consider inverting the condition to return early and avoid nesting (then: %d > after: %d)", thenLen, afterLen)
